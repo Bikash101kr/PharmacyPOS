80a9f9496db9ebc3209a6e4b1a4c13cb254b8bb4
Bikash Dhakal <dhakalbikash0@gmail.com>
2021-03-31 09:53:45 +0545

    final chsges

--- Pharmaceutical-master/Pharmaceutical/Pharmaceutical/Pharmaceutical/View/Login.xaml.cs
+++ Pharmaceutical-master/Pharmaceutical/Pharmaceutical/Pharmaceutical/View/Login.xaml.cs
@@ -18,26 +18,14 @@ namespace Pharmaceutical.View
 	public partial class Login : ContentPage
 	{
         public string ShowUser { get; private set; } // To display which user is log into account 
-        
         public Login ()
 		{
 			InitializeComponent ();
-            
         }
         public UserCRUD userCRUD;
         public Users users;
-
-        
         private async void btn_login(object sender, EventArgs e)
         {
-            //============================================= 
-            // Reference P3: personal assistance 
-            // Purpose: Deal with proper user validation with username and password 
-            // Date: 17 Sep 2018 
-            // Source: fellow student Sandip Dhakal
-            // Assistance: get username saved in database and validate via password for login
-            //============================================= 
-
             try
             {
                  Users users = new Users();
@@ -48,25 +36,13 @@ namespace Pharmaceutical.View
                  {
                      if (Password.Text == u.Password)
                      {
-                         DisplayAlert("Alert", "login succesful", "OK");
+                        await DisplayAlert("Alert", "login succesful", "OK");
 
                          if (u.Role == "Admin")
                          {
                             ShowUser = Username.Text;
-
-                            //============================================= 
-                            // Reference P4: personal assistance 
-                            // Purpose: Deal with proper user role to navigate corresponding page
-                            // Date: 17 Sep 2018 
-                            // Source: fellow student Sandip Dhakal
-                            // Assistance: to get Administrative View
-                            //============================================= 
                             Application.Current.MainPage = new NavigationPage(new AdminView(this));
                          }
-                        //============================================= 
-                        // End reference p4
-                        //============================================= 
-                        else
                         {
                             ShowUser = Username.Text;
                             Application.Current.MainPage = new NavigationPage(new UserView(this));
@@ -84,9 +60,7 @@ namespace Pharmaceutical.View
 
                  throw;
              }
-            //============================================= 
-            // End reference p3
-            //============================================= 
+             
         }
 
     }
